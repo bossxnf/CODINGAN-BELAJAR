b57abe9b9bc16fd0c6dfc5d599efcf39d194decb
bossxnf <136183068+bossxnf@users.noreply.github.com>
2026-04-15 08:00:41 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root","","db_motor");
-
-// cek konksi
-if (mysqli_connect_errno()){
-	echo "koneksi database gagal : " . mysqli_connect_error();
-}
-?>
